c221dd4b91e4bed92067e62b838de4590c1ee4e8
Chad Woolley <thewoolleyman@gmail.com>
2019-01-30 13:30:56 -0700

    initial commit, generated from Spring initializr

---
+++ src/main/kotlin/com/thewoolleyweb/bkautoretry/BkautoretryApplication.kt
@@ -0,0 +1,12 @@
+package com.thewoolleyweb.bkautoretry
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BkautoretryApplication
+
+fun main(args: Array<String>) {
+	runApplication<BkautoretryApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/thewoolleyweb/bkautoretry/BkautoretryApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.thewoolleyweb.bkautoretry
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BkautoretryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
