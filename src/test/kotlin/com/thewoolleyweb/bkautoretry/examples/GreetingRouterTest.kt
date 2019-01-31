551b35ecc4001d6195fffb6718a38e85dfc14118
Chad Woolley <thewoolleyman@gmail.com>
2019-01-31 13:04:42 -0700

    Add @AutoConfigureWebTestClient to fix intellij error

--- src/test/kotlin/com/thewoolleyweb/bkautoretry/examples/GreetingRouterTest.kt
+++ src/test/kotlin/com/thewoolleyweb/bkautoretry/examples/GreetingRouterTest.kt
@@ -4,6 +4,7 @@ package com.thewoolleyweb.bkautoretry.examples
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
@@ -13,6 +14,7 @@ import org.springframework.test.web.reactive.server.expectBody
 @RunWith(SpringRunner::class)
 //  We create a `@SpringBootTest`, starting an actual server on a `RANDOM_PORT`
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class GreetingRouterTest {
 
     // Spring Boot will create a `WebTestClient` for you,
--- src/test/kotlin/com/thewoolleyweb/bkautoretry/hook/GreetingRouterTest.kt
+++ src/test/kotlin/com/thewoolleyweb/bkautoretry/hook/GreetingRouterTest.kt
@@ -4,6 +4,7 @@ package com.thewoolleyweb.bkautoretry.hook
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
@@ -12,6 +13,7 @@ import org.springframework.test.web.reactive.server.expectBody
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class HookRouterTest {
 
     @Autowired
